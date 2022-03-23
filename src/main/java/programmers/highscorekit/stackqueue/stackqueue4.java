0ea6f1231b397216e6f3a03a54446e95bc6e059e
boris <boris0815@naver.com>
2022-03-24 00:51:36 +0900

    Add: highscorekit solution

---
+++ src/main/java/programmers/highscorekit/stackqueue/stackqueue4.java
@@ -0,0 +1,15 @@
+package programmers.highscorekit.stackqueue;
+
+public class stackqueue4 {
+    public static void main(String args[])
+    {
+
+    }
+    public static int[] solution(int[] prices)
+    {
+        int[] answer ={};
+        return answer;
+    }
+
+
+}
