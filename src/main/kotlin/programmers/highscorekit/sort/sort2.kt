d6d3ca19dda4bf10fc9ef58eb3f6b0ca3d30a2ed
boris <boris0815@naver.com>
2022-03-26 22:40:13 +0900

    Add: sort solution
    Add: sort solution

---
+++ src/main/kotlin/programmers/highscorekit/sort/sort2.kt
@@ -0,0 +1,23 @@
+package programmers.highscorekit.sort
+
+/**
+ * @desc
+ *
+ * @input
+ *
+ * @output
+ *
+ * @example
+ *
+ */
+
+fun main() {
+    val numbers = intArrayOf(6, 10, 2)
+    println(solution(numbers))
+}
+
+fun solution(numbers: IntArray): String {
+    var answer = ""
+
+    return answer
+}
