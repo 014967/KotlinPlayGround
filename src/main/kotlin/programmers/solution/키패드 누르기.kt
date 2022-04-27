22924a22e47c65e7805ccd8b80c34b247438a369
HyunKuk Kim <62296097+014967@users.noreply.github.com>
2022-04-27 12:12:32 +0900

    Add: 키패드 누르기
    Add: 키패드 누르기

--- src/main/kotlin/programmers/solution/키패드 누르기.kt
+++ src/main/kotlin/programmers/solution/키패드 누르기.kt
@@ -20,10 +20,26 @@ fun main() {
 class Solution5 {
     fun solution(numbers: IntArray, hand: String): String {
 
-        var answer = StringBuilder()
-        var leftIdx = 0
-        var rightIdx = 0
+        val table = Array(4) { Array(4) { 0 } }
+        /*
+        number = 1 [0][0]
+        number = 2 [0][1]
+        number = 3 [0][2] // 첫번째 인덱스는 4로 나눈 몫, 두번째 인덱스는 나머지 -1
+        number = 4 [1][0] //
+        number = 5 [1][1]
+        number = 6 [1][2]
+        number = 7 [2][0]
+        number = 8 [2][1]
+        number = 9 [2][2]
+        number = * [3][0]
+        number = 0 [3][1]
+        number = # [3][2]
 
+         */
+        var answer = StringBuilder()
+
+        var leftIdx = -1 // *
+        var rightIdx = 10 // #
         for (i in numbers) {
             if (i == 1 || i == 4 || i == 7) {
                 answer.append("L")
@@ -31,21 +47,26 @@ class Solution5 {
             } else if (i == 3 || i == 6 || i == 9) {
                 answer.append("R")
                 rightIdx = i
-            } else {
-                var right_distance = 0
-                var left_distance = 0
+            } else { // 가운데 숫자가 눌릴 때
+                if (leftIdx == -1) {
 
-                if (left_distance == right_distance) {
-                    if (hand == "right") {
-                        answer.append("R")
-                        rightIdx = i
-                    } else {
-                        answer.append("L")
-                        leftIdx = i
-                    }
+                }
+                if (rightIdx == 10) {
                 }
             }
         }
         return answer.toString()
     }
 }
+
+/*
+
+
+        val top = arrayOf(1, 2, 3, 4) // 내가 현재 위치가 1,3일 때, 2,5,8,0에 대한 거리
+        val middle = arrayOf(2, 1, 2, 3) // 내 위치가  4,6일 때 2,5,8,0에 대한 거리
+        val bottom = arrayOf(3, 2, 1, 2) // 내 위치가 7,9일 때 2,5,8,0에 대한 거리
+        val top2 = arrayOf(0, 1, 2, 3) // 내위치가 2
+        val middle5 = arrayOf(1, 0, 1, 2) // 내위치가 5
+        val bottom8 = arrayOf(2, 1, 0, 1) // 내위치가 8
+        val zero = arrayOf(3, 2, 1, 0) // 내위치가 0
+ */
